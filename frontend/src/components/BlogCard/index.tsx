f0086b5cd6ade92ceed60a47b69754c02aa988a2
Aryan Tomar <aryantomar03@gmail.com>
2023-07-07 18:28:06 +0530

    Feat: Changed homepage design

--- frontend/src/components/BlogCard/index.tsx
+++ frontend/src/components/BlogCard/index.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./BlogCard.scss";
 import moment from "moment";
 import { Link } from "react-router-dom";
+import api from "../../hooks/AxiosApi";
+import Skeleton from "@mui/material/Skeleton";
 
 interface BlogCardProps {
   data: {
@@ -15,19 +17,88 @@ interface BlogCardProps {
 }
 
 function BlogCard(props: BlogCardProps) {
+  const [author, setAuthor] = React.useState<any>({});
+
+  const [loading, setLoading] = React.useState<boolean>(true);
+
+  useEffect(() => {
+    api
+      .get(`/getAuthor/${props.data.user_id}`)
+      .then((res) => {
+        setAuthor(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <Link to={`/blog/${props.data.blog_id}`}>
       <div className="BlogCard">
-        <img src={props.data.blog_image} alt="" className="BlogCard__image" />
-        <p className="BlogCard__date">
-          {moment(props.data.blog_date).format("MMMM Do, YYYY")}
-        </p>
-        <h1 className="BlogCard__title">{props.data.blog_title}</h1>
-        <p className="BlogCard__content">
-          {props.data.blog_content.length > 250
-            ? `${props.data.blog_content.substring(0, 100)}...`
-            : props.data.blog_content}
-        </p>
+        <div className="BlogCard__left">
+          <div className="BlogCard__left--author">
+            {loading ? (
+              <Skeleton variant="circular" width={35} height={35} />
+            ) : (
+              <img
+                src={`https://api.dicebear.com/6.x/bottts-neutral/svg?seed=${author[0]?.user_name}`}
+                alt=""
+                className="BlogCard__left--author-image"
+              />
+            )}
+            {loading ? (
+              <Skeleton variant="rectangular" width={200} height={35} />
+            ) : (
+              <div className="BlogCard__left--author-column">
+                <div className="BlogCard__left--author-row">
+                  <div className="BlogCard__left--author-name">
+                    {author[0]?.Name}
+                  </div>
+                  <span className="BlogCard__left--author-span" />
+                  {moment(props.data.blog_date).format("MMMM Do, YYYY")}
+                </div>
+                Founder of chronicles
+              </div>
+            )}
+          </div>
+          <div className="BlogCard__left--contents">
+            <h1 className="BlogCard__left--contents-title">
+              {loading ? (
+                <Skeleton variant="text" sx={{ fontSize: "2rem" }} />
+              ) : (
+                props.data.blog_title
+              )}
+            </h1>
+            {loading ? (
+              <Skeleton variant="rectangular" height={70} width={600} />
+            ) : (
+              <p className="BlogCard__left--contents-content">
+                {props.data.blog_content.length > 300
+                  ? `${props.data.blog_content.substring(0, 300)}...`
+                  : props.data.blog_content}
+              </p>
+            )}
+          </div>
+          <div className="BlogCard__left--tags">
+            {loading ? (
+              <Skeleton variant="rectangular" height={35} width={300} />
+            ) : (
+              <>
+                <span className="BlogCard__left--tags-tag">UI Design</span>
+                <span className="BlogCard__left--tags-tag">AI</span>
+                <span className="BlogCard__left--tags-tag">4 min read</span>
+              </>
+            )}
+          </div>
+        </div>
+        <div className="BlogCard__right">
+          {loading ? (
+            <Skeleton variant="rounded" height={135} width={240} />
+          ) : (
+            <img src={props.data.blog_image} alt="" />
+          )}
+        </div>
       </div>
     </Link>
   );
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -3,6 +3,9 @@ import React from "react";
 import "./Home.scss";
 import BlogCard from "../../components/BlogCard";
 import Navbar from "../../components/Navbar";
+import * as fontAwesomeIcons from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Select from "react-select";
 
 interface HomeProps {
   blogs: [];
@@ -11,15 +14,76 @@ interface HomeProps {
 export default function Home(props: HomeProps) {
   const blogs = props.blogs as [];
 
+  const options = [
+    { value: "following", label: "Following" },
+    { value: "newest", label: "Newest" },
+    { value: "mostPopular", label: "Most Popular" },
+  ];
+
   return (
     <div className="HomePage">
+      <div className="HomePage__left">
+        <div className="HomePage__left--search">
+          <div className="HomePage__left--search-input">
+            <button>
+              <FontAwesomeIcon
+                icon={fontAwesomeIcons.faMagnifyingGlass}
+                className="HomePage__left--search-input-icon"
+                // color="bcbcbc"
+              />
+            </button>
+            <input type="text" placeholder="Search" />
+          </div>
+        </div>
+        <div className="HomePage__left--header">
+          <h1>Articles</h1>
+          <Select options={options} defaultValue={options[0]} />
+        </div>
+        <hr className="HomePage__left--divider" />
+        <div className="HomePage__left--blogs">
+          {blogs.map((blog: any) => {
+            return (
+              <>
+                <BlogCard data={blog} />
+                <hr className="HomePage__left--divider" />
+              </>
+            );
+          })}
+        </div>
+      </div>
+      <div className="HomePage__right">
+        <div className="HomePage__right--interestedPeople">
+          <h1>People you might be interested</h1>
+          <div className="HomePage__right--profile">
+            <div className="HomePage__right--profile-details">
+              <img
+                src="https://api.dicebear.com/6.x/bottts-neutral/svg?seed=thearyanthegr8"
+                alt=""
+              />
+              <div className="HomePage__right--profile-text">
+                <h1>Dummy Name</h1>
+                <p>Software Engineer</p>
+              </div>
+            </div>
+            <div className="HomePage__right--profile-follow">
+              <button>
+                <FontAwesomeIcon
+                  icon={fontAwesomeIcons.faPlus}
+                  className="HomePage__right--profile-icon"
+                />
+                Follow
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
       {/* <hr className="HomePage__divider" /> */}
-      <h1 className="HomePage__title">Featured on Chronicles</h1>
+      {/* <h1 className="HomePage__title">Featured on Chronicles</h1>
       <div className="HomePage__blogs">
         {blogs.map((blog: any) => {
           return <BlogCard data={blog} />;
         })}
-      </div>
+      </div> */}
     </div>
   );
 }
